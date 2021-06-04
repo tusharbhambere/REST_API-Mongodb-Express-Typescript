f6f60e523324ca3a7976fbfdc3b41ad41b306462
Tushar <tushar@applore.in>
2021-06-04 17:26:59 +0530

    item

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -6,7 +6,7 @@ import { Home } from "../models/title.des.image";
 
 
 const router=express.Router()
-//post
+//post~
 router.post("/homecreat",async(req:Request,res:Response)=>{
  const {title,description,photo}= req.body;
  const item= Home.set({title,description,photo});
@@ -20,7 +20,7 @@ router.get("/homeget",async(req:Request,res:Response)=>{
  try {
     const item= await Home.find({});
     return res.status(200).json({
-        data:item,
+        item,
     });
      
  } catch (error) {
@@ -37,7 +37,7 @@ router.post("/addphoto",async(req:Request,res:Response)=>{
     const item= photoInsta.set({image});
     await item.save();
     return res.status(200).json({
-   data:item,
+   item,
     });
    });
    
@@ -45,7 +45,7 @@ router.post("/addphoto",async(req:Request,res:Response)=>{
     try {
        const item= await photoInsta.find({});
        return res.status(200).json({
-           data:item,
+           item,
        });
         
     } catch (error) {
@@ -62,7 +62,7 @@ router.post("/addmeme",async(req:Request,res:Response)=>{
     const item= memePhoto.set({memephoto});
     await item.save();
     return res.status(200).json({
-   data:item,
+   item,
     });
    });
    
@@ -70,7 +70,7 @@ router.post("/addmeme",async(req:Request,res:Response)=>{
     try {
        const item= await memePhoto.find({});
        return res.status(200).json({
-           data:item,
+           item,
        });
         
     } catch (error) {
